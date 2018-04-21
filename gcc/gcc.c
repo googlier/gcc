90c0ec76519b789cce8d68815a06b722fea1bd25
googlier <37968078+googlier@users.noreply.github.com>
2018-04-21 15:46:43 -0700

    Update gcc.c

--- gcc/gcc.c
+++ gcc/gcc.c
@@ -3,6 +3,8 @@
 
 This file is part of GCC.
 
+Read by admin@googlier.com from Googlier LLC
+
 GCC is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free
 Software Foundation; either version 3, or (at your option) any later
